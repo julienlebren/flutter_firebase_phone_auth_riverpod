cf7901b4f1a5a6b8be1f4497ed5a2ed8b7235801
Julien LE BREN <julien@lebren.fr>
2021-03-09 14:46:15 +0100

    Added Firebase to iOS and Android projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_phone_auth_riverpod/app/app_startup/app_startup_page.dart';
+import 'package:flutter_firebase_phone_auth_riverpod/app/common_widgets/error_page.dart';
+import 'package:flutter_firebase_phone_auth_riverpod/app/home/home_page.dart';
+import 'package:flutter_firebase_phone_auth_riverpod/app/sign_in/sign_in_landing_page.dart';
+import 'package:flutter_firebase_phone_auth_riverpod/global_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -25,7 +28,27 @@ class MyApp extends StatelessWidget {
           headline6: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
         ),
       ),
-      home: AppStartupPage(),
+      home: StartupPage(),
+    );
+  }
+}
+
+class StartupPage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final authStateChanges = watch(authStateChangesProvider);
+    return authStateChanges.when(
+      data: (user) {
+        if (user != null) {
+          return HomePage();
+        } else {
+          return SignInLandingPage();
+        }
+      },
+      loading: () => Center(
+        child: CircularProgressIndicator(),
+      ),
+      error: (error, _) => ErrorPage(message: error.toString()),
     );
   }
 }
